321e9c885ca190e30bc050ef4d793d5ff5989f6f
robertchou91 <robertchou91@gmail.com>
2019-08-19 10:57:55 +0800

    Moving some data out of the reroll component.

---
+++ src/components/Hero.js
@@ -0,0 +1,47 @@
+export const data = [
+	{
+		hero5good: 'Iseria',
+		hero_details: [
+			{
+				reason1: 'iseria1',
+				reason2: 'iseria2'
+			}
+		]
+	},
+	{
+		hero5good: 'Vildred',
+		hero_details: [
+			{
+				reason1: 'vildred1',
+				reason2: 'vildred2'
+			}
+		]
+	},
+	{
+		hero5ok: 'Ravi',
+		hero_details: [
+			{
+				reason1: 'ravi1',
+				reason2: 'ravi2'
+			}
+		]
+	},
+	{
+		hero4: 'Angelica',
+		hero_details: [
+			{
+				reason1: 'angelica1',
+				reason2: 'angelica2'
+			}
+		]
+	},
+	{
+		hero4: 'Clarissa',
+		hero_details: [
+			{
+				reason1: 'clarissa1',
+				reason2: 'clarissa2'
+			}
+		]
+	}
+]
