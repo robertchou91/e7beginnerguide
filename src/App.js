7919addc5861581fb69447dda445c9c0e288804c
robertchou91 <robertchou91@gmail.com>
2019-08-30 12:02:22 +0800

    styling navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home';
 import Reroll from './components/Reroll';
 import Equipment from './components/Equipment';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './style.scss';
+import './scss/style.scss';
 
 
 
@@ -15,9 +14,11 @@ class App extends React.PureComponent {
             <Router>
                 <div className="App">
                     <Navbar />
-                    <Route path='/' exact component={Home} />
-                    <Route path='/reroll' component={Reroll} />
-                    <Route path='/equipment' component={Equipment} />
+                    <main>
+                        <Route path='/' exact component={Home} />
+                        <Route path='/reroll' component={Reroll} />
+                        <Route path='/equipment' component={Equipment} />
+                    </main>
                 </div>
             </Router>
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import raphi from '../images/raphi.png';
+import raphi from '../../images/raphi.png';
+import e7logo from '../../images/e7logo.png';
 
 
 class Navbar extends React.PureComponent {
 	render() {
 		return (
-			<div className="nav">
-				<nav>
+			<header className="navbar">
+				<nav className="navbar__navigation">
 					<div className="container">
+						<div></div>
 						<div className="nav__brand">
-							<Link to='/'><img src={raphi} alt="icon" /></Link>
+							<Link to='/'><img className="owner-logo" src={raphi} alt="icon" /></Link>
 						</div>
 						<ul className="nav__list">
 							<li className="nav__list-item"><Link to='/'>Home</Link></li>
@@ -19,7 +21,7 @@ class Navbar extends React.PureComponent {
 						</ul>
 					</div>
 				</nav>
-			</div>
+			</header>
 		);
 	}
 }
