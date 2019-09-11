c8fd393351654b6a23a4fe1041d948b1af2c0b64
robertchou91 <robertchou91@gmail.com>
2019-09-11 14:20:47 +0800

    updating some of the data.

--- src/components/rerolldata.js
+++ src/components/rerolldata.js
@@ -40,8 +40,8 @@ export const herodata = [
 		hero_details: [
 			{
 				hero5fullimg: iseriafull,
-				reason1: "Unique skill set where is is able to fully reset the cooldown of any ally's skill while also granting herself an extra turn.",
-				reason2: "She has the ability to apply a 2 turn Defense Break and Unbuffable. While Unbuffable might not be as useful in PVE, it is an extremely powerful debuff for pvp. Defense Break is universally good in every situation barred bosses that have mechanics that makes debuffing them dangerous."
+				reason1: "Unique skill in the game that is able to reset the cooldown of any ally's skill while also granting herself an extra turn.",
+				reason2: "Her Full Bloom dispels all buffs then applies a 2 turn Defense Break and Unbuffable debuff. While Unbuffable might not be as useful in PVE, it is quite an effective debuff for pvp. Defense Break is universally good in every situation barred bosses that have mechanics that makes debuffing them dangerous."
 			}
 		]
 	},
@@ -51,8 +51,8 @@ export const herodata = [
 		hero_details: [
 			{
 				hero5fullimg: kenfull,
-				reason1: "All-rounder bruiser that is capable of tanking, dealing damage, debuffing (Attack Down, Defense Break, Stun, Burn), and buffing himself with Speed Buff and Vigor (Grants Ken 30% Attack and Defense).",
-				reason2: "One of the best and widely used bruisers in the game when it comes Guild Wars PVP."
+				reason1: "All-rounder bruiser that is capable of tanking, dealing damage, debuffing (Attack Down, Defense Break, Stun, Burn), and self-buffing himself with Speed Buff and Vigor (Grants Ken 30% Attack and Defense).",
+				reason2: "One of the best and most widely used bruisers in the game when it comes Guild Wars PVP."
 			}
 		]
 	},
@@ -74,7 +74,7 @@ export const herodata = [
 			{
 				hero5fullimg: sezfull,
 				reason1: "Very good at farming. Not as good as Vildred but he farms decently fast because of his second skill, Enroach.",
-				reason2: "Despite the nerf to windrider, Sez is still quite good in a cleave team in Arena PVP and Guild Wars but he starts to fall off more and more the higher rank you are in PVP in comparison to other cleave team compositions."
+				reason2: "Despite the nerf to windrider, Sez is still decent in a cleave team in Arena PVP and Guild Wars but he starts to fall off more and more the higher rank you are in PVP in comparison to other cleave team compositions."
 			}
 		]
 	},
@@ -84,8 +84,8 @@ export const herodata = [
 		hero_details: [
 			{
 				hero5fullimg: tywinfull,
-				reason1: "Good ice type knight that can tank the wyvern in Wyvern 11 farm and thereby jumpstarting your progression when it comes to gearing your hero units.",
-				reason2: "Can work in an okay cleave team but is definitely not as reliable as the most meta cleave teams in Arena PVP. Guild Wars has more variety in team compositions so there are times where Tywin can work as a part of a cleave team (The enemy team composition is very slow, and not as tanky as the Arena defense teams)."
+				reason1: "Good ice type knight that can tank the wyvern in Wyvern 11 farm and thereby jumpstarting your progression when it comes to gearing your units.",
+				reason2: "Decent support unit for a cleave team for Arena and Guild Wars PVP (Soul burn for aoe Defense Break and then increasing Combat Readiness to an ally unit)."
 			}
 		]
 	},
@@ -95,7 +95,7 @@ export const herodata = [
 		hero_details: [
 			{
 				hero5fullimg: vildredfull,
-				reason1: "Arguably the best farmer in the game. Farming stages in Epic Seven is important as you level your Phantasma in order to 6 star hero units and Vildred excels in this area. Extremely easy to gear due to Adventure's Path and Labyrinth providing gears that works well with him.",
+				reason1: "Arguably the best farmer in the game. Farming stages in Epic Seven is important as you level your Phantasma in order to 6 star hero units and Vildred excels in this area. Easy to gear due to Adventure's Path and Labyrinth providing gears that works well with him.",
 				reason2: "The only 5 star hero that has speed as his imprint. Very strong in pvp offense when it comes to cleaving."
 			}
 		]
@@ -117,8 +117,8 @@ export const herodata = [
 		hero_details: [
 			{
 				hero4fullimg: arminfull,
-				reason1: "In my opinion one of the best tanks when it comes to clearing Abyss. Shield Wall provides team wide barriers for 3 turns while also healing based on target max hp. Flash of Light provides the needed hit chance down debuffs in abyss to prevent your team from taking too much damage.",
-				reason2: "A decent PVP stall tank that has 10% damage mitigation when Shield Wall is on cooldown."
+				reason1: "In my opinion one of the best tanks when it comes to clearing Abyss. Shield Wall provides team wide barriers for 3 turns while also healing based on target max hp. Flash of Light provides the needed Hit Chance Down debuffs in abyss to prevent your team from taking too much damage. Flash of Light also provides a 3 turn Poison debuff (Poison debuffs are very good in Abyss since enemies and boss HP are very large).",
+				reason2: "An okay PVP stall tank that has 10% damage mitigation when Shield Wall is on cooldown."
 			}
 		]
 	},
@@ -128,8 +128,8 @@ export const herodata = [
 		hero_details: [
 			{
 				hero4fullimg: ciddfull,
-				reason1: "Great PVP unit that has a high damage multiplier skill, Hack, that can attack with elemental advantage on any unit as long as he has Speed Buff on him.",
-				reason2: "Very beginner friendly for new players trying to get into the PVP side of Epic Seven. However specialty changing an easy to acquire 3 star unit, Roozid, will definitely make your pvp progression more smoothly. Righteous Thief Roozid and Cidd synergizes well and is usually the most common combo new players use in PVP."
+				reason1: "Great PVP unit with a high damage multiplier skill, Hack, that can attack with elemental advantage on any unit as long as he has Speed Buff on him.",
+				reason2: "Very beginner friendly for new players trying to get into the PVP side of Epic Seven. Righteous Thief Roozid and Cidd synergizes well and is usually the most common combo new players use in PVP."
 			}
 		]
 	},
@@ -140,7 +140,7 @@ export const herodata = [
 			{
 				hero4fullimg: clarissafull,
 				reason1: "Decent farmer although not as good as Vildred.",
-				reason2: "With a couple molagora investment, she can be a decent secondary DPS as well as a defense breaker."
+				reason2: "With a couple molagora investment, she can be a decent secondary DPS as well as a defense breaker for Wyvern 11 farm."
 			}
 		]
 	},
@@ -150,8 +150,8 @@ export const herodata = [
 		hero_details: [
 			{
 				hero4fullimg: corvusfull,
-				reason1: "Strong Guild War unit that is capable of winning in a 1 v 3 scenario versus certain team compositions. When placed in a defense team composition, opposing opponents are forced to build a team capable of killing him or there is a chance that Corvus can time them out.",
-				reason2: "Having no cooldown on Fury's Advent can make him nearly unkillable provided that he has enough Fighting Spirit to keep using it. Other than incredible sustain, Corvus skill kit is overloaded with Stun debuffs as well as the ability to transfer debuffs to an enemy."
+				reason1: "Strong Guild War unit that is capable of winning in a 1 v 3 scenario versus certain team compositions.",
+				reason2: "Having no cooldown on Fury's Advent can make him nearly unkillable provided that he has enough Fighting Spirit to keep using it. This makes him a pretty decent PVP defense unit as there are times where Corvus alone can time a player out due to being unable to kill him."
 			}
 		]
 	},
@@ -172,7 +172,7 @@ export const herodata = [
 		hero_details: [
 			{
 				hero4fullimg: schurifull,
-				reason1: "Decent Combat Readiness pusher as long as he can crit the enemies. In harder contents like Abyss, Schuri can cycle more turns for the entire team.",
+				reason1: "Decent Combat Readiness pusher as long as he can crit the enemies. In PVE contents like Abyss, Schuri can cycle more turns for the entire team.",
 				reason2: "He has a rather high speed imprint going upwards of 14 speed to the two back slots of the team composition. This is quite useful when it comes to giving the main Combat Readiness pusher the speed needed to go first at the start of PVP."
 			}
 		]
